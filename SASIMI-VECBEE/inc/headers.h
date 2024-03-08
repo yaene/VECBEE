7a961c892de301ff55425afe4cd85701a7c09acc
yaene <yannis.bolliger@protonmail.com>
2024-03-08 12:19:52 +0000

    fix: filesystem header experimental in older compilers

--- SASIMI-VECBEE/inc/headers.h
+++ SASIMI-VECBEE/inc/headers.h
@@ -1,23 +1,29 @@
 #ifndef HEADERS_H
 #define HEADERS_H
 
-
-#include <boost/random.hpp>
-#include <boost/multiprecision/cpp_int.hpp>
 #include <boost/multiprecision/cpp_dec_float.hpp>
+#include <boost/multiprecision/cpp_int.hpp>
+#include <boost/random.hpp>
+#ifdef __cpp_lib_filesystem
 #include <filesystem>
+#else
+#include <experimental/filesystem>
+namespace std {
+namespace filesystem = experimental::filesystem;
+}
+#endif
+#include <experimental/filesystem>
 #include <iostream>
 #include <limits.h>
 #include <list>
 #include <random>
+#include <set>
 #include <sstream>
 #include <stdint.h>
 #include <unordered_map>
 
-
 #define IN
 #define OUT
 #define INOUT
-
 
 #endif
